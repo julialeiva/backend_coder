d1201fdc0b3b1c189f9ae246ad2e8be27d443b8d
María Julia Leiva <julia.leiva93@gmail.com>
2023-02-15 22:22:58 -0300

    H y WS

---
+++ src/controller/ViewManager.js
@@ -0,0 +1,18 @@
+import ProductManager from "../controller/ProductManager.js"
+
+const prodManager = new ProductManager
+
+class ViewsManager {
+  async renderHome(req, res) {
+    const prods = await prodManager.getProducts()
+    res.render('home', {
+      hasProds: prods.length > 0,
+      prods
+    })
+  }
+  async renderProds(req, res) {
+    res.render('realTimeProducts')
+  }
+}
+
+export default ViewsManager
--- src/index.js
+++ src/index.js
@@ -1,24 +1,63 @@
 import express from "express";
+import { engine } from "express-handlebars";
 import cartRouter from "./routes/cartRouter.js";
 import productsRouter from "./routes/productsRouter.js";
+import viewsRouter from "./routes/viewsRouter.js";
+import * as path from "path"
+import { __dirname } from './utils/path.js'
+import { Server } from "socket.io";
+import ProductManager from "./controller/ProductManager.js";
 
+const prodManager = new ProductManager
 
 //Configuration
 const app = express()
 const PORT = 8080
 
-//Middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
-//Static
-app.use(express.static('public'));
-
-//Routes
-app.use('/api/carts', cartRouter)
-app.use('/api/products', productsRouter)
-
 //Server
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`\n\tServidor encendido y escuchando en el puerto ${PORT}.\n\tVisita http://localhost:${PORT}`);
 })
+
+const io = new Server(server);
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", path.resolve(__dirname, "../views"))
+
+io.on('connection', (e) => {
+  e.on('getProListUpdated', async () => {
+    const prods = await prodManager.getProducts()
+    e.emit('proListUpdated', JSON.stringify(prods))
+  })
+  e.on('addProd', async (data) => {
+    try {
+      await prodManager.addProduct(JSON.parse(data))
+      const prods = await prodManager.getProducts()
+      socket.emit('proListUpdated', JSON.stringify(prods))
+      socket.emit('addProdRes', JSON.stringify({ isOk: true, msg: `Producto agregado satisfactoriamente.` }))
+    } catch (error) {
+      socket.emit('addProdFRes', JSON.stringify({ isOk: false, msg: `Hubo un error al agregar un producto.`, valid: [] }))
+    }
+  })
+  e.on('rmProd', async (id) => {
+    try {
+      await prodManager.deleteProduct(id)
+      const prods = await prodManager.getProducts()
+      socket.emit('proListUpdated', JSON.stringify(prods))
+      socket.emit('rmProdError', JSON.stringify({ isOk: true, msg: `Producto eliminado satisfactoriamente.` }))
+    } catch (error) {
+      socket.emit('rmProdError', JSON.stringify({ isOk: false, msg: `Hubo un error al agregar un producto.` }))
+    }
+  })
+  return e
+})
+
+//Routes
+app.use('/static', express.static(path.join(__dirname + '/public'))) //Static
+app.use('/', viewsRouter) //Views
+app.use('/api/carts', cartRouter) //API
+app.use('/api/products', productsRouter) //API
